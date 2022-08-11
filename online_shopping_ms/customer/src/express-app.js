f183e25059cc13e130f017dac4aff90506d0d0be
Dima Fiialo <dafw@live.ru>
2022-08-12 00:50:07 +0300

    splitting mono to dif ms

--- online_shopping_ms/customer/src/express-app.js
+++ online_shopping_ms/customer/src/express-app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors  = require('cors');
-const { customer, products, shopping } = require('./api');
+const { customer } = require('./api');
 const HandleErrors = require('./utils/error-handler');
 
 
 module.exports = async (app) => {
-
     app.use(express.json({ limit: '1mb'}));
     app.use(express.urlencoded({ extended: true, limit: '1mb'}));
     app.use(cors());
@@ -13,10 +12,7 @@ module.exports = async (app) => {
 
     //api
     customer(app);
-    products(app);
-    shopping(app);
 
     // error handling
     app.use(HandleErrors);
-    
 }
